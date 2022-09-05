a2fed74b8eb805001b7011a9455d9c20fcb9013b
git-shahid26 <102609484+git-shahid26@users.noreply.github.com>
2022-09-05 18:28:00 +0530

    Add files via upload

---
+++ ncHelloworld.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    printf("Hello and Welcome to golden Touch smart Graduates program of new College....");
+
+
+}
---
+++ ncHelloworld3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a;
+    float b;
+    printf("please enter a int and float...:");
+    scanf("%d%f",&a,&b);
+    printf("The a value is..:%d and b value is...:%f",a,b);
+
+
+}
