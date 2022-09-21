ffb17d4fdfc7eef9d691dec8bf1c484259c6580b
git-shahid26 <102609484+git-shahid26@users.noreply.github.com>
2022-09-21 21:40:42 +0530

    Add files via upload

---
+++ Add Two Numbers.c
@@ -0,0 +1,10 @@
+//Add Two Number
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter two number to add\n");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("Sum of entered numbers=%d\n",c);
+}
---
+++ C Input Integer 2.c
@@ -0,0 +1,12 @@
+//Input Integer 2
+#include<stdio.h>
+int main()
+{
+    int number;
+
+    printf("please enter a number:");
+    scanf("%d",&number);
+    printf("You entered %d",number);
+
+    return 0;
+}
