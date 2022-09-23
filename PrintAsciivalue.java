dd3acb884902137e7c8e0d9e0d4c2bfbd159a152
git-shahid26 <102609484+git-shahid26@users.noreply.github.com>
2022-09-23 20:34:50 +0530

    Add files via upload

---
+++ PrintAsciivalue.java
@@ -0,0 +1,19 @@
+package smartgraduates;
+
+public class PrintAsciivalue {
+	public static void main(String[] args) 
+	{
+		char ch='H';
+		
+		int ascii=ch;
+		int castascii=(int) ch;
+		System.out.println("The ASCII value of "+ch+" is: "+ascii);
+		System.out.println("The ASCII value of "+ch+" is: "+castascii);
+		
+		
+		
+		
+	
+	}
+
+}
