f0ae87596687a06bff800aecbac22c5321694c23
git-shahid26 <102609484+git-shahid26@users.noreply.github.com>
2022-09-23 20:36:09 +0530

    Add files via upload

---
+++ CastingExercise.java
@@ -0,0 +1,20 @@
+package smartgraduates;
+
+public class CastingExercise {
+	public static void main(String[] args) {
+		byte b=10;
+		
+		int i=b;
+		
+		byte c=i;
+		
+		byte d=(byte)i;
+		
+		byte X=10;
+		byte Y=20;
+		
+		byte sum=X*Y;
+	}
+	
+
+}
