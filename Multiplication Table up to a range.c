80a48c2386c408680d43134cdd4f6c333e9c212d
git-shahid26 <102609484+git-shahid26@users.noreply.github.com>
2022-10-04 23:52:34 +0530

    Add files via upload

---
+++ Multiplication Table up to a range.c
@@ -0,0 +1,20 @@
+//Multiplication Table up to a range
+#include<stdio.h>
+int main(){
+    int n,i,range;
+    printf("Enter the integer:");
+    scanf("%d",&n);
+
+    do{
+        printf("Enter the range(positive integer):");
+        scanf("%d",&range);
+    }
+    while(range<=0);
+
+    for(i=1;i<=range;++i){
+        printf("%d * %d=%d\n",n,i,n*i);
+    }
+
+    return 0;
+
+}
